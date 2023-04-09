c8e62660880ef15983113911576cf90d978c29a5
FengDenny <fengdennyy@gmail.com>
2023-04-08 20:29:50 -0700

    Added hero slider images data

---
+++ src/data/hero.ts
@@ -0,0 +1,27 @@
+import Hero1 from "@/assets/hero/hero_1.jpg"
+import Hero2 from "@/assets/hero/hero_2.jpg"
+import Hero3 from "@/assets/hero/hero_3.jpg"
+import Hero4 from "@/assets/hero/hero_4.jpg"
+
+export const sliderImg = [
+    {
+    id: 1,
+    title: "Front yard landscape 1",
+    img:Hero1
+}, 
+{
+    id: 2,
+    title: "Front yard landscape 2 ",
+    img: Hero2
+}, 
+{
+    id: 3,
+    title: "Front yard landscape 3 ",
+    img: Hero3
+}, 
+{
+    id: 4,
+    title: "Back yard landscape ",
+    img: Hero4
+}
+]
