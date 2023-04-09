7fa7917f23447772b170a9edd65c6cb75e05764f
FengDenny <fengdennyy@gmail.com>
2023-04-08 20:29:33 -0700

    Create navLinks.ts

---
+++ src/data/navLinks.ts
@@ -0,0 +1,28 @@
+export const navLinks = [
+    {
+      id:"0",
+      title: "家",
+      href:"/"
+    },
+    {
+      id:"1",
+      title: "圖庫館",
+      href:"/gallery"
+    },
+    {
+      id:"2",
+      title:"風水園林",
+      href:"/fengshuigarden"
+    },
+    {
+      id:"3",
+      title:"關于",
+      href:"/about"
+    },
+    {
+      id:"4",
+      title: "聯系",
+      href:"/contact"
+    }
+  ];
+  
