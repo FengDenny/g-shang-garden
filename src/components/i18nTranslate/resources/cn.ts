73e494bba1101f4c177cb13138d53ad698ad5e3a
FengDenny <fengdennyy@gmail.com>
2023-04-10 14:33:20 -0700

    Added service/offer section translations data

--- src/components/i18nTranslate/resources/cn.ts
+++ src/components/i18nTranslate/resources/cn.ts
@@ -1,4 +1,6 @@
 import {cnNavLinks} from "@/data/navLinks"
+import { cnServices } from "@/data/services";
+import { cnDesign, cnExperience } from "@/data/offer";
 
 export const cn = {
   navigation: cnNavLinks,
@@ -6,5 +8,20 @@ export const cn = {
   hero:{
     title: "景观美化理念变为现实",
     description: "聯系電話: (00) 415-678-6636 ｜ 408-686-2888"
+  },
+  service: {
+    title: "服务",
+    header:"景观和花园解决方案",
+    preview:"看预览",
+    services: cnServices
+  },
+  offer:{
+    title:"提供",
+    header:"當您選擇我們時我們提供什麼",
+    designHeader:"設計力",
+    design: cnDesign,
+    experienceHeader:"經驗力",
+    experience: cnExperience,
   }
   };
+ 
--- src/components/i18nTranslate/resources/en.ts
+++ src/components/i18nTranslate/resources/en.ts
@@ -1,4 +1,6 @@
 import {enNavLinks} from "@/data/navLinks"
+import { enServices } from "@/data/services";
+import { enDesign, enExperience } from "@/data/offer";
 
 
 export const en = {
@@ -7,6 +9,20 @@ export const en = {
   hero:{
     title: "Landscaping ideas turned into reality",
     description: "Telephone: (00) 415-678-6636 ｜ 408-686-2888"
+  },
+  service: {
+    title: "Services",
+    header:"Landscape and Garden Solutions ",
+    preview:"See preview",
+    services: enServices,
+  },
+  offer:{
+    title:"Offer",
+    header:"What We Offer When You Choose Us",
+    designHeader:"Design Power",
+    design: enDesign,
+    experienceHeader:"Experience",
+    experience: enExperience,
   }
   };
   
