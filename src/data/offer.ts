51f87cbaca1c0fccd6cd824e095cc0c8fd0f21c4
FengDenny <fengdennyy@gmail.com>
2023-04-10 14:32:39 -0700

    Create offer.ts

---
+++ src/data/offer.ts
@@ -0,0 +1,70 @@
+export const enDesign = [{
+        id: 0,
+        title: "Excellent aesthetic sense",
+    },
+    {
+        id: 1,
+        title: "High-quality material package guarantee",
+    },
+    {
+        id: 2,
+        title: "Proven Construction Team",
+    }
+
+]
+
+export const enExperience = [{
+        id: 0,
+        title: "Decades of garden design experience",
+    },
+    {
+        id: 1,
+        title: "Rich Feng Shui knowledge",
+    },
+    {
+        id: 2,
+        title: "A good after-sales service",
+    } , {
+        id: 3,
+        title: "Warm service attitude",
+    } , {
+        id: 4,
+        title: "High word-of-mouth communication in the Chinese community",
+    }
+
+]
+export const cnDesign = [{
+        id: 0,
+        title: "卓越的審美感",
+    },
+    {
+        id: 1,
+        title: "質量過硬的材料包保證",
+    },
+    {
+        id: 2,
+        title: "久經考驗的施工團",
+    }
+
+]
+
+export const cnExperience = [{
+        id: 0,
+        title: "數十年園林設計經驗",
+    },
+    {
+        id: 1,
+        title: "豐富的風水學知識",
+    },
+    {
+        id: 2,
+        title: "完善的售后服務",
+    } , {
+        id: 3,
+        title: "熱情的服務態度",
+    } , {
+        id: 4,
+        title: "華人社區高口碑傳播",
+    }
+
+]
