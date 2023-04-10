8977d3699573935af595e02f0a029f3bd65d38e2
FengDenny <fengdennyy@gmail.com>
2023-04-10 14:32:37 -0700

    Create services.ts

---
+++ src/data/services.ts
@@ -0,0 +1,67 @@
+import e_landscape from "@/assets/services/e_landscape.svg"
+import feng_shui from "@/assets/services/feng_shui.svg"
+import garden from "@/assets/services/garden.svg"
+import symbiosis from "@/assets/services/symbiosis.svg"
+
+export const enServices = [
+    {
+        id:0,
+        title: "Modern Feng Shui",
+        description:"The purpose of Feng Shui is to find vitality. A lively place should be a place sheltered from the wind and facing the sun, with beautiful mountains and clear waters, gurgling water, lush vegetation, singing birds and dancing birds, and the fragrance of birds and flowers.",
+        svg:feng_shui,
+        preview: "/gallery"
+    },
+   {
+        id:1,
+        title: "Environmental Landscape",
+        description:"Various physical, physiological and psychological effects of environmental landscape on human beings. All of these will make us healthier physically and mentally, clearer and more agile in thinking, and more active in creating inspiration. Therefore, garden geomantic omen has been paid more and more attention by people.",
+        svg:e_landscape,
+        preview: "/gallery"
+    },
+   {
+        id:2,
+        title: "Symbosis",
+        description:"Use garden landscaping, including the orientation, size, flow direction of water features, sand cultivation in mountain views, plant cultivation, vigorous growth of trees, tree species, distribution of roads in the park, etc., to achieve the proper position of landscape in Feng Shui, in line with ecological nature.",
+        svg: symbiosis,
+        preview: "/gallery"
+    },   
+    {
+        id:3,
+        title: "Chinese Garden",
+        description:"As a part of ancient Chinese traditional culture, Chinese garden culture has a great influence on world gardening culture. Gardens are a form of architectural art. It essentially aims to organize an environment full of temperament, fun and artistic conception.",
+        svg: garden,
+        preview: "/gallery"
+    },
+]
+
+export const cnServices = [
+    {
+        id:0,
+        title: "現代風水學",
+        description:"風水的宗旨就是尋找生氣。而有生氣的地方應該是避風向陽，山清水秀，流水潺潺,草木欣欣，莺歌燕舞，鳥語花香之地。",
+        svg:feng_shui,
+        preview: "/gallery"
+    },
+   {
+        id:1,
+        title: "環境景觀",
+        description:"環境景觀對人類的各種生理、生理和心理影響。所有這些都會讓我們身心更加健康，思維更加清晰敏捷，更加積極地創造靈感。因此，園林風水越來越受到人們的重視。",
+        svg:e_landscape,
+        preview: "/gallery"
+    },
+   {
+        id:2,
+        title: "共生共存",
+        description:"利用園林造景，包括水景的方位、大小、流向、山景養沙、植物栽培、樹木旺盛、樹種、園內道路分佈等，達到造景的恰到好處的位置在風水上，符合生態自然。",
+        svg: symbiosis,
+        preview: "/gallery"
+    },   
+    {
+        id:3,
+        title: "中國園林",
+        description:"中國園林文化作為中國古老傳統文化的一部分，對世界園林文化產生了重大影響。園林是建築藝術的一種形式。其本質目的是營造一個充滿氣質、情趣和意境的環境。",
+        svg: garden,
+        preview: "/gallery"
+    },
+
+]
