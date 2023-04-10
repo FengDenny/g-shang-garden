e1c95682b48b5169ff11461bc0d6ba1b8f61a62e
FengDenny <fengdennyy@gmail.com>
2023-04-10 09:59:31 -0700

    Removed  top positioning, height as it caused layout issue / unnecessary divs

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -17,44 +17,40 @@ const index = (props: Props) => {
     cssEase: "cubic-bezier(0.000, 0.000, 0.635, 0.750)",
   };
   return (
-    <section className="relative ">
-      <div className=" absolute -top-32  mt-32 h-40 w-full">
-        <div className="relative">
-          <div className="absolute z-10 w-full">
-            <h2
-              className="relative z-10 mx-auto text-center font-extrabold uppercase text-color-white-100 
+    <section className="relative">
+      <div className="absolute z-10 w-full">
+        <h2
+          className="relative z-10 mx-auto text-center font-extrabold uppercase text-color-white-100 
             below768:mt-28 below768:text-xl
             xs:w-9/12
             sm:mt-40 sm:w-11/12 sm:text-3xl
             md:mt-56 md:text-4xl"
-            >
-              {translate("hero.title")}
-              <span className="text-primary-green-100 md:text-2xl">.</span>
-            </h2>
-            <h6
-              className="mx-auto mt-5
+        >
+          {translate("hero.title")}
+          <span className="text-primary-green-100 md:text-2xl">.</span>
+        </h2>
+        <h6
+          className="mx-auto mt-5
             text-center text-lg text-primary-yellow-100 xs:mt-1 xs:w-8/12 xs:text-sm sm:w-11/12"
-            >
-              {translate("hero.description")}
-            </h6>
-          </div>
-
-          <Slider {...settings}>
-            {sliderImg.map((items: any) => {
-              const { id, title, img } = items;
-              return (
-                <img
-                  src={img}
-                  key={id}
-                  alt={title}
-                  className=" h-64
-                   w-full  brightness-50 below397:h-72 sm:h-max"
-                />
-              );
-            })}
-          </Slider>
-        </div>
+        >
+          {translate("hero.description")}
+        </h6>
       </div>
+
+      <Slider {...settings}>
+        {sliderImg.map((items: any) => {
+          const { id, title, img } = items;
+          return (
+            <img
+              src={img}
+              key={id}
+              alt={title}
+              className=" h-64
+                   w-full  brightness-50 below397:h-72 sm:h-max"
+            />
+          );
+        })}
+      </Slider>
     </section>
   );
 };
