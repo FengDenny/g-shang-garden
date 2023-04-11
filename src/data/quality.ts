9e1c6105bd41aac928b7e34e9fb7d38eb371aa51
FengDenny <fengdennyy@gmail.com>
2023-04-11 13:11:25 -0700

    Added picture data for quality section

---
+++ src/data/quality.ts
@@ -0,0 +1,27 @@
+import q1 from "@/assets/quality/q_1.png"
+import q2 from "@/assets/quality/q_2.png"
+import q3 from "@/assets/quality/q_3.png"
+import q4 from "@/assets/quality/q_4.png"
+
+export const sliderImg = [
+    {
+    id: 1,
+    title: "Garden quality",
+    img:q1
+}, 
+{
+    id: 2,
+    title: "Tiles quality ",
+    img: q2
+}, 
+{
+    id: 3,
+    title: "Front yard tile quality ",
+    img: q3
+}, 
+{
+    id: 4,
+    title: "Back yard tile quality ",
+    img: q4
+}
+]
