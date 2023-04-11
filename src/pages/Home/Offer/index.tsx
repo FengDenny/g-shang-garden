9edee2ffbd35ab6edf0bee1d6a6a7b9186a16a2c
FengDenny <fengdennyy@gmail.com>
2023-04-11 13:26:26 -0700

    Refactored offer to own component  for reusability

--- src/pages/Home/Offer/index.tsx
+++ src/pages/Home/Offer/index.tsx
@@ -3,12 +3,13 @@ import { translate, translateObjArr } from "@/components/i18nTranslate/helper";
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 import Header from "@/components/SectionHeader";
+import Offer from "@/components/Offer";
 
 type Props = {};
 
 const index = (props: Props) => {
-  const design = translateObjArr("offer.design");
-  const experience = translateObjArr("offer.experience");
+  const design: Array<any> = translateObjArr("offer.design");
+  const experience: Array<any> = translateObjArr("offer.experience");
 
   return (
     <section className=" mt-30 mx-auto flex  w-3/6 p-10 xs:w-full ">
@@ -22,48 +23,11 @@ const index = (props: Props) => {
           />
         </div>
         <div className="">
-          <div className="relative mt-10">
-            <h2 className="text-xl font-bold text-primary-dark-300 underline decoration-primary-green-300 underline-offset-8 below397:text-base ">
-              {translate("offer.designHeader")}
-            </h2>
-            <div className="mt-8 grid grid-cols-2-320 gap-10 below768:grid-cols-1">
-              {design.map((items: any) => {
-                const { id, title } = items;
-
-                return (
-                  <div className="flex flex-row" key={id}>
-                    <span className="text-2xl text-primary-green-300">
-                      <MdOutlineKeyboardDoubleArrowRight />
-                    </span>
-                    <h5 className="font-semibold text-primary-dark-100">
-                      {title}
-                    </h5>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="relative mt-10">
-            <h2 className="text-xl font-bold text-primary-dark-300 underline decoration-primary-green-300 underline-offset-8 below397:text-base">
-              {translate("offer.experienceHeader")}
-            </h2>
-            <div className="mt-8 grid grid-cols-2-320 gap-10 below768:grid-cols-1">
-              {experience.map((items: any) => {
-                const { id, title } = items;
-
-                return (
-                  <div className="flex flex-row" key={id}>
-                    <span className="text-2xl text-primary-green-300">
-                      <MdOutlineKeyboardDoubleArrowRight />
-                    </span>
-                    <h5 className="font-semibold text-primary-dark-100">
-                      {title}
-                    </h5>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          <Offer design={design} headerTranslate={"offer.designHeader"} />
+          <Offer
+            design={experience}
+            headerTranslate={"offer.experienceHeader"}
+          />
         </div>
       </div>
     </section>
