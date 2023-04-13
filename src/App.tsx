8270bceb3ae76ca8ca4213ee82ee564a7bde8bfb
FengDenny <fengdennyy@gmail.com>
2023-04-13 12:01:59 -0700

    Refactored scroll event listener to use  IntersectionObserver / removed boolean prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import AnimatedRight from "@/components/Navbar/AnimatedRight";
-import { useRoutes, Navigate } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import Home from "@/pages/Home";
 
 function App() {
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
 
   const stickyFooterStyling = "flex h-screen flex-col justify-between";
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY == 0) {
-        setIsTopOfPage(true);
-      }
-
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   const Main = () => {
     let routes = useRoutes([
@@ -35,11 +21,7 @@ function App() {
 
   return (
     <div className={`${stickyFooterStyling} app `}>
-      <Navbar
-        isTopOfPage={isTopOfPage}
-        isMenuToggled={isMenuToggled}
-        setIsMenuToggled={setIsMenuToggled}
-      >
+      <Navbar isMenuToggled={isMenuToggled} setIsMenuToggled={setIsMenuToggled}>
         <AnimatedRight
           isMenuToggled={isMenuToggled}
           setIsMenuToggled={setIsMenuToggled}
