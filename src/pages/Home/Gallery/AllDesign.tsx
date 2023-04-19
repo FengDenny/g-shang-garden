7620d0211493e5f39a51840e2832c7659eb0567b
FengDenny <fengdennyy@gmail.com>
2023-04-19 10:57:07 -0700

    All Design tab gallery created

---
+++ src/pages/Home/Gallery/AllDesign.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PagePagination from "@/components/Pagination/PagePagination";
+
+type Props = {};
+
+const AllDesign = (props: Props) => {
+  return <PagePagination path={"all design"} />;
+};
+
+export default AllDesign;
