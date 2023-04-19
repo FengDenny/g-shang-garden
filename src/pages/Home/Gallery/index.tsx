154f07f1207e69d3d08bd316efa965713dcea35b
FengDenny <fengdennyy@gmail.com>
2023-04-19 10:58:11 -0700

    Added Tabs for Gallery page to show  All Designs, Construction, Quality tab content

--- src/pages/Home/Gallery/index.tsx
+++ src/pages/Home/Gallery/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import PagePagination from "@/components/Pagination/PagePagination";
+import React, { useState } from "react";
+import { GalleryTabs } from "@/data/tabs";
+import Tabs from "@/components/Tabs";
 
 type Props = {};
 
 const index = (props: Props) => {
+  const [selectedTab, setSelectedTab] = useState(GalleryTabs[0].id);
   return (
-    <div className="mx-auto mt-40 w-4/6">
-      <PagePagination path={"all design"} />
+    <div className="mx-auto mt-40 w-[90%] p-4">
+      <Tabs
+        tabs={GalleryTabs}
+        selectedTab={selectedTab}
+        onClick={setSelectedTab}
+      />
     </div>
   );
 };
