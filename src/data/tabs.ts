0aa7c17a9ef5f4e49da44839098773ce2b218a82
FengDenny <fengdennyy@gmail.com>
2023-04-19 10:56:48 -0700

    Gallery tabs data created

---
+++ src/data/tabs.ts
@@ -0,0 +1,21 @@
+import AllDesign from "@/pages/Home/Gallery/AllDesign";
+import Construction from "@/pages/Home/Gallery/Construction";
+import Quality from "@/pages/Home/Gallery/Quality";
+
+export 
+const GalleryTabs = [
+  {
+    id: 1,
+    label: "All Design",
+    Component: AllDesign,
+  },
+  {
+    id: 2,
+    label: "Construction",
+    Component: Construction,
+  },  {
+    id: 3,
+    label: "Quality Materials",
+    Component: Quality,
+  },
+];
