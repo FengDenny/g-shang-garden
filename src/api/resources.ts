3c31b7ba49ba879a03b6c8bef099e81746250e74
FengDenny <fengdennyy@gmail.com>
2023-04-18 17:16:17 -0700

    Cloudinary API resources endpoint created

---
+++ src/api/resources.ts
@@ -0,0 +1,22 @@
+import { Buffer } from "buffer";
+Buffer.from("anything", "base64");
+
+export const imgResources = async (folder: string) => {
+    const response = await fetch(
+      `/api/${
+        import.meta.env.VITE_C_CLOUD_NAME
+      }/resources/by_asset_folder?asset_folder=g shang garden/${folder}&max_results=500`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            import.meta.env.VITE_C_API_KEY +
+              ":" +
+              import.meta.env.VITE_C_API_SECRET
+          ).toString("base64")}`,
+        },
+      }
+    )
+      .then((result) => result.json())
+      .catch((err) => console.log(err));
+    return response;
+  };
