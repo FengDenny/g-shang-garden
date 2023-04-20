5c15fd73d0c546ec64ecd395f6c2da82f03c54ad
FengDenny <fengdennyy@gmail.com>
2023-04-20 13:39:01 -0700

    Added gallery page title

--- src/pages/Home/Gallery/index.tsx
+++ src/pages/Home/Gallery/index.tsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
-import { GalleryTabs } from "@/data/tabs";
+// import { GalleryTabs } from "@/data/tabs";
 import Tabs from "@/components/Tabs";
+import { translate, translateTabsObj } from "@/components/i18nTranslate/helper";
+import PageTitle from "@/components/Helmet";
 
 type Props = {};
 
 const index = (props: Props) => {
-  const [selectedTab, setSelectedTab] = useState(GalleryTabs[0].id);
+  const GalleryTabs = translateTabsObj("gallery.tabs");
+  const [selectedTab, setSelectedTab] = useState<number>(GalleryTabs[0].id);
+
   return (
-    <div className="mx-auto mt-40 w-[90%] p-4">
-      <Tabs
-        tabs={GalleryTabs}
-        selectedTab={selectedTab}
-        onClick={setSelectedTab}
-      />
-    </div>
+    <>
+      <PageTitle pageTitle={translate("gallery.title")} />
+      <section className="mx-auto mt-40 w-[90%] p-4">
+        <Tabs
+          tabs={GalleryTabs}
+          selectedTab={selectedTab}
+          onClick={setSelectedTab}
+        />
+      </section>
+    </>
   );
 };
 
