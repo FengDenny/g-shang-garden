d72f4fa85c1ed1779f28f70070be5b3ca36a7a49
FengDenny <fengdennyy@gmail.com>
2023-04-24 13:02:19 -0700

    Added en/cn form validation

--- src/components/helper/FormValidation.ts
+++ src/components/helper/FormValidation.ts
@@ -1,15 +1,21 @@
+export const FormValidation = (lang:string, nameType : string, name : string, id:string , setError: (value: string) => void) => {  
 
-export const formValidation = (nameType : string, name : string, id:string , setError: (value: string) => void) => {
     nameType && document.getElementById(id)?.addEventListener("keyup", function() {
     const nameRegex = /^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$/;
 
     const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
 
     let nameInput =(document.getElementById(id) as HTMLInputElement)?.value.trim()
+
+    console.log(name)
+
     if (!nameInput){
-        setError(`${name} is required.`);
-    }else if (name === "Full name" && !nameType.match(nameRegex) || name === "Email" && !nameType.match(emailRegex)) {
-        setError(`${name} is invalid.`);
+      lang === "en" ?  setError(`${name} is required.`) :  setError(`${name} 是必须的.`)
+    }else if (
+    (name === "Full name" || name ==="全名") && !nameType.match(nameRegex)
+    || (name === "Email address" || name === "电子邮件地址") && !nameType.match(emailRegex)
+    ) {
+      lang === "en" ?  setError(`${name} is invalid.`) :  setError(`${name} 是无效的.`)
       } else {
         setError("");
       }
