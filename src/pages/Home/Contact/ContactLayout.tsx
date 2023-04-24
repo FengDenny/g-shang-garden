841fd99db9a75149fd0c2561f6ee3fff532a5038
FengDenny <fengdennyy@gmail.com>
2023-04-24 14:55:49 -0700

    Added contact Info component

--- src/pages/Home/Contact/ContactLayout.tsx
+++ src/pages/Home/Contact/ContactLayout.tsx
@@ -1,6 +1,7 @@
 import { Ref } from "react";
 import { LabelWithInput, SendButton } from "@/components/Contact";
 import { translate } from "@/components/i18nTranslate/helper";
+import ContactInfo from "@/components/Contact/ContactInfo";
 
 type Props = {
   isLoading: boolean;
@@ -49,66 +50,74 @@ const ContactLayout = ({
         </h2>
         <span className="border-b-color-description mx-auto mt-1 w-44 border-b border-solid"></span>
         <p
-          className="description-text relative left-6  
-        mx-auto mt-4 w-64 text-xl 
+          className="description-text relative 
+        mx-auto mt-4 w-11/12 text-xl 
         font-normal text-primary-dark-100 
         sm:w-96"
         >
           {translate("contact.description")}
         </p>
       </div>
-      <div
-        className="mx-auto mt-2 flex h-auto  w-5/6 flex-col  flex-wrap justify-between py-24 text-primary-dark-300 transition-colors
+      <div>
+        <div
+          className="mx-auto mt-2 flex h-auto  w-4/6 flex-col  flex-wrap justify-between py-24 text-primary-dark-300 transition-colors
              delay-100 duration-100 ease-in-out sm:flex-row"
-      >
-        <div className="mx-auto">
-          <form
-            ref={formRef}
-            className="delay-70 animate-fadeIn  relative mx-auto  
-          mt-10 flex h-auto w-80 flex-col rounded-lg 
-          bg-primary-green-300 p-10   
-          shadow-lg  transition-colors
-          duration-75 ease-in-out sm:-top-6 sm:left-5
+        >
+          <div className="mt-10 p-5 sm:mx-auto">
+            <ContactInfo />
+          </div>
+          <div className="sm:mx-auto">
+            <form
+              ref={formRef}
+              className="delay-70 animate-fadeIn  relative mt-10  
+          flex h-auto w-11/12 flex-col rounded-lg bg-primary-green-300 
+          p-10 shadow-lg   
+          transition-colors  duration-75
+          ease-in-out sm:-top-6 sm:left-5 sm:mx-auto
           sm:w-full
           "
-          >
-            <LabelWithInput
-              id="fullName"
-              htmlFor="Full Name"
-              labelName={translate("contact.fullName")}
-              type="text"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-              error={fullNameError}
-            />
-            <LabelWithInput
-              id="email"
-              htmlFor="Email Address"
-              labelName={translate("contact.email")}
-              value={email}
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              error={emailError}
-            />
-            <LabelWithInput
-              id="message"
-              htmlFor="Message"
-              type="message"
-              labelName={translate("contact.message")}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              error={messageError}
-            />
-            <SendButton
-              enabled={enabled}
-              messageSent={messageSent}
-              isLoading={isLoading}
-              fullName={fullName}
-              message={message}
-              email={email}
-              handleSend={handleSend}
-            />
-          </form>
+            >
+              <h2 className="text-lg font-bold text-color-white">
+                Send us an email
+              </h2>
+              <LabelWithInput
+                id="fullName"
+                htmlFor="Full Name"
+                labelName={translate("contact.fullName")}
+                type="text"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                error={fullNameError}
+              />
+              <LabelWithInput
+                id="email"
+                htmlFor="Email Address"
+                labelName={translate("contact.email")}
+                value={email}
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                error={emailError}
+              />
+              <LabelWithInput
+                id="message"
+                htmlFor="Message"
+                type="message"
+                labelName={translate("contact.message")}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                error={messageError}
+              />
+              <SendButton
+                enabled={enabled}
+                messageSent={messageSent}
+                isLoading={isLoading}
+                fullName={fullName}
+                message={message}
+                email={email}
+                handleSend={handleSend}
+              />
+            </form>
+          </div>
         </div>
       </div>
     </section>
