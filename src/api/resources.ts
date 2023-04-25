f05531609facd846f9fd95178c9961a6da938461
FengDenny <fengdennyy@gmail.com>
2023-04-24 17:07:22 -0700

    Update resources.ts

--- src/api/resources.ts
+++ src/api/resources.ts
@@ -4,16 +4,11 @@ export const imgResources = async (folder: string) => {
   const auth = {login: '137827112126634', password: 'MxHh3jlJZfALgTNxlBnCCCdrh1g'} 
 
     const response = await fetch(
-      `/api/
-        dis7ep3yq
-      /resources/by_asset_folder?asset_folder=g shang garden/${folder}&max_results=500`,
+      `/api/dis7ep3yq/resources/by_asset_folder?asset_folder=g shang garden/${folder}&max_results=500`,
       {
         headers: {
           Authorization: `Basic ${Buffer.from(
-            auth.login +
-              ":" +
-              auth.password
-          ).toString("base64")}`,
+            auth.login +":"+auth.password).toString("base64")}`,
         },
       }
     )
