8a90d20b2585a4a940533a6fc9f7346aa59e9bd2
FengDenny <fengdennyy@gmail.com>
2023-04-24 22:29:06 -0700

    Update resources.ts

--- src/api/resources.ts
+++ src/api/resources.ts
@@ -1,25 +1,28 @@
 import { Buffer } from "buffer";
+import axios from "axios";
 
 export const imgResources = async (folder: string) => {
 
-  console.log( import.meta.env.VITE_C_CLOUD_NAME,   import.meta.env.VITE_C_API_KEY + ":" + import.meta.env.VITE_C_API_SECRET)
+  
+  const token = Buffer.from(
+    import.meta.env.VITE_C_API_KEY +
+    ":" +
+    import.meta.env.VITE_C_API_SECRET
+    ).toString("base64")
+    console.log( import.meta.env.VITE_C_CLOUD_NAME,  token)
 
-    const response = await fetch(
+    const response = await axios.get(
       `/api/${
         import.meta.env.VITE_C_CLOUD_NAME
       }/resources/by_asset_folder?asset_folder=g shang garden/${folder}&max_results=500`,
       {
         headers: {
-          Authorization: `Basic ${Buffer.from(
-            import.meta.env.VITE_C_API_KEY +
-              ":" +
-              import.meta.env.VITE_C_API_SECRET
-          ).toString("base64")}`,
+          Authorization: `Basic ${token}`,
         },
       }
     )
       .then((result) => {
-        return result.json()
+        return result.data
       })
       .catch((err) => console.log(err));
 
